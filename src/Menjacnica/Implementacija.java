23190233ec423ae766c1efbaf88e2342bf51a254
AleksaT <aleksa.topalovic015@gmail.com>
2015-03-25 15:35:57 +0100

    Kreirana je klasa Implementacija koja implementira interface MenjacnicaInterfejs.

---
+++ src/Menjacnica/Implementacija.java
@@ -0,0 +1,25 @@
+package Menjacnica;
+
+import java.util.GregorianCalendar;
+
+import Interfejs.MenjacnicaInterfejs;
+
+public class Implementacija implements MenjacnicaInterfejs {
+
+	public void dodavanjeKursa(GregorianCalendar datum, String valutaIme,
+			double prodajni, double srednji, double kupovni) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void brisanjeKursa(GregorianCalendar datum, String valutaIme) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void pronadjiKurs(GregorianCalendar datum, String valutaIme) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
