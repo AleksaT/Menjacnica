07621cd066c03778c1472a9111680186421a7c1f
AleksaT <aleksa.topalovic015@gmail.com>
2015-03-27 20:32:22 +0100

    Implementacija interfejsa

--- src/Menjacnica/Implementacija.java
+++ src/Menjacnica/Implementacija.java
@@ -8,17 +8,17 @@ public class Implementacija implements MenjacnicaInterfejs {
 
 	public void dodavanjeKursa(GregorianCalendar datum, String valutaIme,
 			double prodajni, double srednji, double kupovni) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	public void brisanjeKursa(GregorianCalendar datum, String valutaIme) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	public void pronadjiKurs(GregorianCalendar datum, String valutaIme) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
